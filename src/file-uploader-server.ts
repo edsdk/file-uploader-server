f49c0672463ffd64a10e5ab8306d957ee54da267
Dmitriy Komarov <komarov-dmitriy@yandex.ru>
2019-05-03 16:19:45 +0300

    API call added

--- src/file-uploader-server.ts
+++ src/file-uploader-server.ts
@@ -2,69 +2,7 @@ import * as express from "express";
 import * as Busboy from "busboy";
 import {UploaderServlet} from "servlet/UploaderServlet";
 
-class ExpressFileUploader {
-
-    protected servlet: UploaderServlet;
-
-    constructor(
-        protected app: express.Express,
-        protected url: string,
-        protected config: {[key: string]: string}
-    ) {
-        this.servlet = new UploaderServlet(config);
-        app.post(url, (request: express.Request, response: express.Response) => {
-            this.upload(request, response);
-        });
-        app.options(url, (request: express.Request, response: express.Response) => {
-            this.uploadOptions(request, response);
-        });
-    }
-
-    private upload(request: express.Request, response: express.Response): void {
-        let busboy = new Busboy({ "headers": request.headers });
-        busboy.on('file', function(fieldname: string, file: any, filename: string, encoding: string, mimetype: string) {
-            if (fieldname === "file") {
-                (request as any)["postFile"] = {
-                    "filename": filename,
-                    "data": null
-                };
-                file.on('data', function (data: Buffer) {
-                    let oldData: Buffer = (request as any)["postFile"]["data"];
-                    let newData = oldData == null ? data : Buffer.concat([oldData, data]);
-                    (request as any)["postFile"]["data"] = newData;
-                });
-            }
-        });
-        busboy.on('field', (fieldname: string, val: any, fieldnameTruncated: string, valTruncated: any, encoding: string, mimetype: string) => {
-            if (fieldname === "data")
-                (request as any).postData = val;
-        });
-        busboy.on('finish', () => {
-            this.servlet.doPost(request, response);
-        });
-        request.pipe(busboy);
-    }
-
-    private uploadOptions(request: express.Request, response: express.Response) {
-        this.servlet.doOptions(request, response);
-        response.sendStatus(200);
-    }
-
-}
-
-export function bindFileUploader(config: {
-    app: express.Express,
-    url: string,
-    dir: string,
-    config?: {[key: string]: string}
-}) {
-    if (!config.config)
-        config.config = {};
-    config.config['dirFiles'] = config.dir;
-    new ExpressFileUploader(config.app, config.url, config.config);
-}
-
-export interface FileUploaderMicroserviceConfig {
+export interface FileUploaderMicroserviceOptions {
     host?: string,        // host to listen
     port?: number,        // port to listen
     urlUploader?: string, // path only
@@ -73,7 +11,7 @@ export interface FileUploaderMicroserviceConfig {
     config?: {[key: string]: string} // config to pass into file-uploader-server
 }
 
-export function startFileUploaderMicroservice(config: FileUploaderMicroserviceConfig): express.Express {
+export function startFileUploaderMicroservice(config: FileUploaderMicroserviceOptions): express.Express {
 
     // Create Express app
     let app = express();
@@ -99,4 +37,59 @@ export function startFileUploaderMicroservice(config: FileUploaderMicroserviceCo
     );
 
     return app;
+}
+
+export function bindFileUploader(params: {
+    app: express.Express,
+    url: string,
+    dir: string,
+    config?: {[key: string]: string}
+}) {
+    if (!params.config)
+        params.config = {};
+    params.config['dirFiles'] = params.dir;
+
+    params.app.post(params.url, (request: express.Request, response: express.Response) => {
+        processFileUploaderRequest(request, response, params.config);
+    });
+    params.app.options(params.url, (request: express.Request, response: express.Response) => {
+        processFileUploaderRequest(request, response, params.config);
+    });
+}
+
+export function processFileUploaderRequest(
+    request: express.Request,
+    response: express.Response,
+    config: {[key: string]: string}
+) {
+    let servlet = new UploaderServlet(config);
+
+    if (request.method === "OPTIONS") {
+        servlet.doOptions(request, response);
+        response.sendStatus(200);
+        return;
+    }
+
+    let busboy = new Busboy({ "headers": request.headers });
+    busboy.on('file', function(fieldname: string, file: any, filename: string, encoding: string, mimetype: string) {
+        if (fieldname === "file") {
+            (request as any)["postFile"] = {
+                "filename": filename,
+                "data": null
+            };
+            file.on('data', function (data: Buffer) {
+                let oldData: Buffer = (request as any)["postFile"]["data"];
+                let newData = oldData == null ? data : Buffer.concat([oldData, data]);
+                (request as any)["postFile"]["data"] = newData;
+            });
+        }
+    });
+    busboy.on('field', (fieldname: string, val: any, fieldnameTruncated: string, valTruncated: any, encoding: string, mimetype: string) => {
+        if (fieldname === "data")
+            (request as any).postData = val;
+    });
+    busboy.on('finish', () => {
+        servlet.doPost(request, response);
+    });
+    request.pipe(busboy);
 }
